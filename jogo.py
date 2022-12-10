ebf3783ebd6564c6634375c8ffd7a87977bf3264
JOAO PEDRO COSTA SANTOS <joao.santos@conbral.local>
2022-12-10 15:08:00 -0300

    second commit 2

--- jogo.py
+++ jogo.py
@@ -10,11 +10,6 @@ try:
         pyautogui.write('League of Legends')  # abre o lol
         pyautogui.hotkey('enter')
         pyautogui.hotkey('winleft', '2')  # abre o spotify
-        pyautogui.hotkey('winleft', '6')  # abre o acces
-        time.sleep(5)
-        pyautogui.click(x=438, y=513)
-        pyautogui.hotkey('winleft', '5')  # abre o banco de dados
-        pyautogui.click(x=855, y=1044)  # abre os logs
 
     def pos():
         pyautogui.alert('Não mexa em nada durante a execução')
